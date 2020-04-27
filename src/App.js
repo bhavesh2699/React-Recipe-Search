cc063ab07b34319aa1ccc8487138a24ef79918a1
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-04-28 02:14:29 +0530

    search bar added and API config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+
+	const APP_KEY="7b58dcf7fd0d292afccffd94912fc6fa";
+	const APP_ID="ef399f99";
+
+	useEffect(()=>{
+
+	},[]);
+
+	const getRecipes= async ()=>{
+		const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+		const data=response.json();
+		console.log(data);
+	}
+
+	return(
+
+		<div className="App">
+		<form className="search-form">
+		<input className="search-bar" type="text"/>
+		<button className="search-button" type="submit">search</button>
+		</form>
+
+
+		</div>
+
+	);
 }
 
 export default App;
